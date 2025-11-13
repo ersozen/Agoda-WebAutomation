2541a276da5381d6b81d77db1dadc6e46202ae78
ersin.ozen <ersinozenn@gmail.com>
2025-11-13 12:53:46 +0300

    Login Page Tests commit

--- src/test/java/com/web/base/pages/LoginPage.java
+++ src/test/java/com/web/base/pages/LoginPage.java
@@ -9,8 +9,6 @@ import org.apache.commons.logging.LogFactory;
 public class LoginPage {
     private static final Log log = LogFactory.getLog(LoginPage.class);
     private static BeinTextBox TXT_Email=new BeinTextBox(PageElementModel.selectorNames.CSS_SELECTOR,"input[data-cy=\"unified-email-input\"]\n");
-    private static BeinButton BTN_Account=new BeinButton(PageElementModel.selectorNames.XPATH,"//*[@id=\"128a08a9a7ea88\"]");
-    private static BeinButton BTN_AccountPhone=new BeinButton(PageElementModel.selectorNames.XPATH,"//*[@id=\"ef6ca5a4a8c5c\"]/div/div[1]/div");
     private static BeinTextBox TXT_Password=new BeinTextBox(PageElementModel.selectorNames.ID,"pass");
     public static String USER_EMAIL = System.getenv("USER_EMAIL");
     public static String USER_PASSWORD = System.getenv("USER_PASSWORD");
